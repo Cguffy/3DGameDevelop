b4abbf8911dceb27ceeb406dd6645253f9b19c9d
Cguffy <348950915@qq.com>
2020-09-20 14:59:10 +0800

    hm02

---
+++ 第2次作业/hm02_shortAnswerQuestions/Assets/Q5.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Q5 : MonoBehaviour
+{
+    public Transform table;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject t1 = Instantiate<Transform> (table, this.transform).gameObject;
+		t1.transform.position = new Vector3 (0, 0, 0);
+    }
+}
